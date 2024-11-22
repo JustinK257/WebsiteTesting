97f616018cab2eaa0ef1efcfc8be7e657e7d02a7
Justin Kuoch <justinkuoch257@gmail.com>
2024-11-22 13:51:50 -0800

    folder test

--- homepage.js
+++ homepage.js
@@ -7,29 +7,29 @@ function search(){
   }
   
   function newlistmore(){
-    
+    window.location.href = 'otherhtmls/morelist.html'
   }
   
   function vonstrip(){
-    
+    window.location.href = 'vontrip.html'
   }
   
   function walmarttrip(){
-    
+    window.location.href = 'waltrip.html'
   }
   
   function tripmore(){
-    
+    window.location.href = 'moretrip.html'
   }
   
   function targetsale(){
-    
+    window.location.href = 'target.html'
   }
   
   function ralphscoupon(){
-    
+    window.location.href = 'ralphs.html'
   }
   
   function sandcmore(){
-    
+    window.location.href = 'sale.html'
   }

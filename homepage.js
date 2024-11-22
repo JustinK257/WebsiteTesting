05320c1cc15473545e81a768c0e41809c23950c5
Justin Kuoch <justinkuoch257@gmail.com>
2024-11-21 21:41:26 -0800

    homepage

---
+++ homepage.js
@@ -0,0 +1,35 @@
+function search(){
+    window.location.href = 'itemsearch.html';
+  }
+  
+  function newliststart(){
+    window.location.href = 'newlist.html';
+  }
+  
+  function newlistmore(){
+    
+  }
+  
+  function vonstrip(){
+    
+  }
+  
+  function walmarttrip(){
+    
+  }
+  
+  function tripmore(){
+    
+  }
+  
+  function targetsale(){
+    
+  }
+  
+  function ralphscoupon(){
+    
+  }
+  
+  function sandcmore(){
+    
+  }

d2d4b2bca565e6d3c4c07df62576dbcbd5438b70
Justin Kuoch <justinkuoch257@gmail.com>
2024-12-06 13:15:12 -0800

    Button and navigation update

--- homepage.js
+++ homepage.js
@@ -1,35 +1,35 @@
 function search(){
-    window.location.href = 'itemsearch.html';
+    navigateToPage('itemsearch.html');
   }
   
   function newliststart(){
-    window.location.href = 'newlist.html';
+    navigateToPage('newlist.html');
   }
   
   function newlistmore(){
-    window.location.href = 'otherhtmls/morelist.html'
+    navigateToPage('otherhtmls/morelist.html');
   }
   
   function vonstrip(){
-    window.location.href = 'otherhtmls/vontrip.html'
+    navigateToPage('otherhtmls/vontrip.html');
   }
   
   function walmarttrip(){
-    window.location.href = 'otherhtmls/waltrip.html'
+    navigateToPage('otherhtmls/waltrip.html');
   }
   
   function tripmore(){
-    window.location.href = 'otherhtmls/moretrip.html'
+    navigateToPage('otherhtmls/moretrip.html');
   }
   
   function targetsale(){
-    window.location.href = 'otherhtmls/target.html'
+    navigateToPage('otherhtmls/target.html');
   }
   
   function ralphscoupon(){
-    window.location.href = 'otherhtmls/ralphs.html'
+    navigateToPage('otherhtmls/ralphs.html');
   }
   
   function sandcmore(){
-    window.location.href = 'otherhtmls/sale.html'
+    navigateToPage('otherhtmls/sale.html');
   }
